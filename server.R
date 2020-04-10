d61c5431f212e54c30721187f58b7509b82ac438
Chiarini <daniele@chiarini.de>
2020-04-10 15:34:11 +0200

    First grapph

--- server.R
+++ server.R
@@ -9,7 +9,7 @@ library(ggplot2)
 
 #load("analytics.Rdata") # load the dataframe
 
-load("RPT_ID10_IWOG046MSuspP52Marzo2020.Rdata") # load the dataframe
+(load("RPT_ID10_IWOG046MSuspP52Marzo2020.RData")) # load the dataframe
 
 #G046Msusp <- read.csv2("RPT_ID10_IWOG046MSuspP52Marzo2020.csv")
 #G048MEurope <- read.csv2("RPT_ID10_IWOG048MEuropeP52Marzo2020.csv")
@@ -24,50 +24,49 @@ shinyServer(function(input, output) { # server is defined within these parenthes
   
   passData <- reactive({
     
-    analytics <- analytics[analytics$Date %in% seq.Date(input$dateRange[1], input$dateRange[2], by = "days"),]
+#    analytics <- analytics[analytics$Date %in% seq.Date(input$dateRange[1], input$dateRange[2], by = "days"),]
     
 #    analytics <- analytics[analytics$Hour %in% as.numeric(input$minimumTime) : as.numeric(input$maximumTime),]
     
-    if(class(input$regionShow)=="character"){
+#    if(class(input$regionShow)=="character"){
       
-      analytics <- analytics[analytics$Domain %in% unlist(input$regionShow),]
+#      analytics <- analytics[analytics$Domain %in% unlist(input$regionShow),]
       
-    }
+#      RPT_ID10_IWOG046MSuspP52Marzo2020 <- RPT_ID10_IWOG046MSuspP52Marzo2020[unlist(input$regionShow),]
+      
+#    }
     
-    analytics
+    RPT_ID10_IWOG046MSuspP52Marzo2020
     
-  })
+   })
   
   output$monthGraph <- renderPlot({
     
-    graphData <- ddply(passData(), .(Domain, Date), numcolwise(sum))
+    graphData <- ddply(passData(), .(Anno_Mese, Amount_TOT, c_AmountCluster), numcolwise(sum))
     
-    if(input$outputType == "G351"){
+#    if(input$outputType == "G351"){
       
-      theGraph <- ggplot(graphData, aes(x = Date, y = visitors, group = Domain, colour = Domain)) + geom_line() +
-        ylab("Unique visitors")
+      theGraph <- ggplot(graphData, aes(x = Anno_Mese, y = Amount_TOT, group = c_AmountCluster, colour = c_AmountCluster)) + geom_line()
       
-    }
+#    }
     
-    if(input$outputType == "G048"){
+#    if(input$outputType == "G048"){
+#      
+#      theGraph <- ggplot(graphData, aes(x = Anno_Mese, y = Amount_TOT, group = c_AmountCluster, colour = c_AmountCluster)) + geom_line()
       
-      theGraph <- ggplot(graphData, aes(x = Date, y = bounces / visits * 100, group = Domain, colour = Domain)) +
-        geom_line() + ylab("Bounce rate %")
-      
-    }
+#    }
     
-    if(input$outputType == "G046"){
+#    if(input$outputType == "G046"){
       
-      theGraph <- ggplot(graphData, aes(x = Anno_Mese, y = Amount_TOT, group = c_AmountCluster, colour = c_AmountCluster)) +
-        geom_line() + ylab("Average time on site")
+#      theGraph <- ggplot(graphData, aes(x = Anno_Mese, y = Amount_TOT, group = c_AmountCluster, colour = c_AmountCluster)) + geom_line()
       
-    }
+#    }
     
-    if(input$smoother){
+#    if(input$smoother){
       
-      theGraph <- theGraph + geom_smooth()
+#      theGraph <- theGraph + geom_smooth()
       
-    }
+#    }
     
     print(theGraph)
     
@@ -75,46 +74,43 @@ shinyServer(function(input, output) { # server is defined within these parenthes
   
   output$hourGraph <- renderPlot({
     
-    graphData = ddply(passData(), .(Domain, Hour), numcolwise(sum))
+    graphData = ddply(passData(), .(Anno_Mese, Amount_TOT, c_AmountCluster), numcolwise(sum))
     
-    if(input$outputType == "G351"){
+#    if(input$outputType == "G351"){
       
-      theGraph <- ggplot(graphData, aes(x = Anno_Mese, y = Amount_TOT, group = c_AmountCluster, colour = c_AmountCluster)) + geom_line() +
-        ylab("Unique visitors")
+      theGraph <- ggplot(graphData, aes(x = Anno_Mese, y = Amount_TOT, group = c_AmountCluster, colour = c_AmountCluster)) + geom_line()
       
-    }
+#    }
     
-    if(input$outputType == "G048"){
+#    if(input$outputType == "G048"){
       
-      theGraph <- ggplot(graphData, aes(x = Anno_Mese, y = Amount_TOT, group = c_AmountCluster, colour = c_AmountCluster)) +
-        geom_line() + ylab("Bounce rate %")
+#      theGraph <- ggplot(graphData, aes(x = Anno_Mese, y = Amount_TOT, group = c_AmountCluster, colour = c_AmountCluster)) + geom_line()
       
-    }
+#    }
     
-    if(input$outputType == "G046"){
+#    if(input$outputType == "G046"){
       
-      theGraph <- ggplot(graphData, aes(x = Anno_Mese, y = Amount_TOT, group = c_AmountCluster, colour = c_AmountCluster)) +
-        geom_line() + ylab("Average time on site")
+#      theGraph <- ggplot(graphData, aes(x = Anno_Mese, y = Amount_TOT, group = c_AmountCluster, colour = c_AmountCluster)) + geom_line()
       
-    }
+#    }
         
-    if(input$smoother){
+#    if(input$smoother){
       
-      theGraph <- theGraph + geom_smooth()
+#      theGraph <- theGraph + geom_smooth()
       
-    }
+#    }
     
     print(theGraph)
     
   })
   
   
-  output$textDisplay <- renderText({ 
-    
-    paste(
-      length(seq.Date(input$dateRange[1], input$dateRange[2], by = "days")),
-      " days are summarised. There were", sum(passData()$visitors), "visitors in this time period."
-    )
-    
-  })
+#  output$textDisplay <- renderText({ 
+#    
+#    paste(
+#      length(seq.Date(input$dateRange[1], input$dateRange[2], by = "days")),
+#      " days are summarised. There were", sum(passData()$Amount_TOT), "visitors in this time period"
+#    )
+#    
+#  })
 })
